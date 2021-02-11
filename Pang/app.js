600e515d0fbfece63ecd4c22283eb9e813e677e3
myCom <macchoichoi>
2021-02-12 07:05:38 +0900

    commit

---
+++ Pang/app.js
@@ -0,0 +1,24 @@
+const eventsCenter = new Phaser.Events.EventEmitter();
+const config = {
+    type: Phaser.AUTO,
+    parent: 'phaser',
+    title: "PANG",
+    version: "0.0.1",
+    backgroundColor: '#000000',
+    scene: [GameScene,JoystickScene],
+    scale: {
+        mode: Phaser.Scale.FIT,
+        parent: 'phaser',
+        width: 1920,
+        height: 1040,
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    },
+    pixelArt: true,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: true,
+        }
+    },
+};
+const game = new Phaser.Game(config);
---
+++ Pang/scenes/GameScene.js
@@ -0,0 +1,81 @@
+class GameScene extends Phaser.Scene{
+    constructor(){
+        super({
+            key:'GameScene',
+            active:true
+        });
+        this.gameWidth = game.config.width;
+        this.gameHeight = game.config.height;
+        this.countHarpoon = 0;
+    }
+
+    preload(){
+        this.load.setPath('./assets/images');
+        this.load.image('bg', 'bg/bg.png');
+        this.load.image('player', 'player/0.png');
+        this.load.image('harpoon', 'harpoon/1.png');
+        this.load.image('ball', 'ball/0.png');
+    }
+
+    create(){
+        this.physics.world.setBoundsCollision(true, true, true, true);
+        this.gameBounds = new Phaser.Geom.Rectangle(30, 30, this.gameWidth-60, this.gameHeight-60);
+        this.add.image(0, 0, 'bg').setOrigin(0, 0).setScale(5);
+
+        this.player = this.physics.add.sprite(this.gameWidth/2, this.gameHeight-120, 'player').setScale(5);
+        this.player.setCollideWorldBounds(true);
+        this.player.setImmovable(true);
+        this.player.body.customBoundsRectangle = this.gameBounds;
+
+        eventsCenter.on('keyup', this.onKeyCode, this);
+        this.groupBall = this.add.group();
+
+        this.addBall(500,0,5);
+    }
+    
+    onKeyCode (event){
+        switch (event.keyCode) {
+            case Phaser.Input.Keyboard.KeyCodes.LEFT:
+                this.player.setVelocityX(-300, 10);
+            break;
+            case Phaser.Input.Keyboard.KeyCodes.RIGHT:
+                this.player.setVelocityX(300, 10);
+               break;
+            case Phaser.Input.Keyboard.KeyCodes.A:
+                this.fireHarpoon();
+            break  
+        }
+    }
+
+    fireHarpoon(){
+        if(this.countHarpoon>2)return;
+        this.countHarpoon++;
+        var harpoon=this.physics.add.sprite(this.player.x, this.player.y, 'harpoon').setScale(5);
+        var collider=this.physics.add.collider(harpoon, this.groupBall, this.harpoonHit, null, this);
+        this.tweens.add({
+            targets:harpoon,
+            y:0,
+            duration:2000,
+            onComplete:function(tween,tergets){
+                this.countHarpoon--;
+                harpoon.destroy();
+                this.physics.world.removeCollider(collider);
+            }.bind(this)
+        })
+    }
+
+    harpoonHit(harpoon,targets) {
+        if(targets.scale>1)
+        this.addBall(targets.x,targets.y,targets.scale-=1);
+        harpoon.destroy();
+        targets.destroy();
+     
+        if(this.groupBall.children.size == 0)
+        this.addBall(500,0,5);
+    }
+
+    addBall(x,y,scale){
+        this.groupBall.add(new Ball(this,x,y,"ball",1,scale));
+        this.groupBall.add(new Ball(this,x,y,"ball",-1,scale));
+    }
+}
