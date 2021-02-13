37694e7218064a737d49f03b629ebe8d95fd9913
chocho <diskbox1@daum.net>
2021-02-13 16:53:59 +0900

    commit

---
+++ Pang/assets/images/player/sprites.js
@@ -0,0 +1,35 @@
+{
+"frames": {
+	"1.png": {
+		"frame": {"x":0, "y":0, "w":31, "h":30},
+		"spriteSourceSize": {"x":0,"y":0,"w":31,"h":30},
+		"sourceSize": {"w":31,"h":30}
+	},
+	"2.png": {
+		"frame": {"x":0, "y":31, "w":28, "h":31},
+		"spriteSourceSize": {"x":0,"y":0,"w":28,"h":31},
+		"sourceSize": {"w":28,"h":31}
+	},
+	"3.png": {
+		"frame": {"x":32, "y":0, "w":27, "h":30},
+		"spriteSourceSize": {"x":0,"y":0,"w":27,"h":30},
+		"sourceSize": {"w":27,"h":30}
+	},
+	"4.png": {
+		"frame": {"x":29, "y":31, "w":28, "h":31},
+		"spriteSourceSize": {"x":0,"y":0,"w":28,"h":31},
+		"sourceSize": {"w":28,"h":31}
+	},
+	"5.png": {
+		"frame": {"x":58, "y":31, "w":25, "h":31},
+		"spriteSourceSize": {"x":0,"y":0,"w":25,"h":31},
+		"sourceSize": {"w":25,"h":31}
+	}
+
+},
+"meta": {
+	"image": "sprites.png",
+	"size": {"w": 84, "h": 63},
+	"scale": "1"
+}
+}
