a2e0927738de8e5c4e045aa1478af40ac9db24fd
chocho <diskbox1@daum.net>
2021-03-14 22:25:31 +0900

    commit

--- WhiteBoard/PWA/sw.js
+++ WhiteBoard/PWA/sw.js
@@ -1,34 +1,36 @@
 var CACHE_NAME = 'starcraft-phaser-v1';
 var filesToCache = [
-  '/',
-  '/index.html',
-  '/libray/phaser.min.js',
-  '/MainScene.js',
-  '/assets/images/brush.png',
-  '/manifest.json',
-  '/PWA/pwa.js',
-  '/PWA/sw.js',
-  '/PWA/images/icon_192x192.png',
-  '/PWA/images/icons_512x512.png',
+  './index.html',
+  './libray/phaser.min.js',
+  './MainScene.js',
+  './assets/images/brush.png',
+  './manifest.json',
+  './PWA/pwa.js',
+  './PWA/sw.js',
+  './PWA/images/icon_192x192.png',
+  './PWA/images/icons_512x512.png',
 ];
 
 self.addEventListener('install', function(event) {
     // self.skipWaiting();
     console.log('app install');
     event.waitUntil(
-        caches.keys().then(function(keyList) {
-            return Promise.all(keyList.map(function(key) {
-                if (key !== CACHE_NAME) {
-                    console.log('app removing old cache', key);
-                    return caches.delete(key);
-                }
-            }));
+        caches.open(filesToCache).then(cache => {
+            return cache.addAll(filesToCache);
         })
     );
 });
 
 self.addEventListener('activate', event => {
-    console.log('now ready to handle fetches!');
+    console.log('Activating new service worker...');
+    caches.keys().then(function(keyList) {
+        return Promise.all(keyList.map(function(key) {
+            if (key !== CACHE_NAME) {
+                console.log('app removing old cache', key);
+                return caches.delete(key);
+            }
+        }));
+    })
 });
 
 // 요청에 실패하면 오프라인 페이지 표시
