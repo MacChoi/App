c5558e04efb99b95a31d5684ff1b9dfcc8b139cb
chocho <diskbox1@daum.net>
2021-04-05 18:14:21 +0900

    ColorClock

--- Gom/MainScene.js
+++ Gom/MainScene.js
@@ -6,13 +6,125 @@ class MainScene extends Phaser.Scene {
     }
 
     preload(){
-        this.load.image('gom', 'assets/images/1.png');
 
+        this.load.image('test', ['assets/images/face/1.png', 'assets/images/face/2.png']);
+        this.load.image('face', 'assets/images/face/face.png');
+        this.load.image('eye1', 'assets/images/face/eye1.png');
+        this.load.image('eye2', 'assets/images/face/eye2.png');
+        this.load.image('mouth', 'assets/images/face/mouth.png');
+        this.load.image('tongue', 'assets/images/face/tongue.png');
+    
+        this.wavebell = new WaveBell();
     }
 
+    
     create(){
-        this.add.image(WIDTH/2, HEIGHT/2, 'gom');
+        const app = new PIXI.Application({
+            width: WIDTH, height: HEIGHT,
+            view: document.getElementById('canvas_1')});
+     
+        var img = new PIXI.Sprite.from('assets/images/face/1.png');
+        img.width = WIDTH/2;
+        img.height = HEIGHT/2;
+        img.x=100;
+        var depthMap = new PIXI.Sprite.from('assets/images/face/2.png');
+        depthMap.width = WIDTH/2;
+        depthMap.height = HEIGHT/2;
+        depthMap.x=100;
+        app.stage.addChild(img);
+        app.stage.addChild(depthMap);
 
+        var displacementFilter = new PIXI.filters.DisplacementFilter(depthMap);
+        app.stage.filters = [displacementFilter];
+    
+        this.input.on('pointermove', function (pointer)  {
+            // console.log("pointermove" , pointer)
+                displacementFilter.scale.x = (window.innerWidth / 2 - pointer.event.clientX) /20;
+                displacementFilter.scale.y = (window.innerHeight / 2 - pointer.event.clientY) /20;
+        }.bind(this));
+
+
+        this.tweens.addCounter({
+            from: -200,
+            to: 200,
+            duration: 1500,
+            yoyo:true,
+            repeat:-1,
+            onUpdate:function(tween){
+                displacementFilter.scale.x = (window.innerWidth / 2 - tween.getValue()) /20;
+                // displacementFilter.scale.y = (window.innerHeight / 2 - Phaser.Math.Between(-300,300)) /20;
+            }
+        });
+
+        this.tweens.addCounter({
+            from: -200,
+            to: 200,
+            duration: 5000,
+            yoyo:true,
+            repeat:-1,
+            onUpdate:function(tween){
+                // displacementFilter.scale.x = (window.innerWidth / 2 - tween.getValue()) /20;
+                displacementFilter.scale.y = (window.innerHeight / 2 - tween.getValue()) /20;
+            }
+        });
+
+        var x=WIDTH/2;
+        var y=HEIGHT/2 +100;
+
+        this.face= this.add.image(0,0, 'face');
+        var eye1= this.add.image(-13,-13, 'eye1');
+        var eye2= this.add.image(15,-12, 'eye2');
+        this.mouth= this.add.image(0,8, 'mouth').setOrigin(0.5,0);
+        // this.tongue= this.add.image(0,55, 'tongue');
+
+        this.container = this.add.container(x,y);
+
+        var shuffleTest = [ this.face, eye1, eye2 ,this.mouth ,];
+        
+        this.container.add(shuffleTest);
+     
+
+        this.tweens.add({
+            targets: [eye1],
+            scaleY: 0.1,
+            ease: 'Circ.easeInOut',
+            duration: 3000,
+            yoyo:true,
+            repeat: -1,
+        });
+
+        this.input.on('pointerdown', function (pointer) {
+            if (this.wavebell.state === 'inactive') {
+                this.wavebell.start(1000 / 25);
+            }
+
+        }.bind(this));
+
+        this.wavebell.on('wave', function (e) {
+            // if(e.value * 100 < 10)return;
+            console.log(Math.round(e.value * 100) + '%')
+            // this.tongue.setScale(3,3);
+           
+            this.mouth.scaleY=e.value*3;
+            // // if(this.ani!=undefined)this.ani.stop();
+            // this.ani=this.tweens.add({
+            //     // targets: Phaser.Math.RND.shuffle(shuffleTest),
+            //     targets:this.mouth,
+            //     // angle: 5,
+            //     scaleY:1,
+            //     ease: 'Circ.easeInOut',
+            //     duration: 3000,
+            //     yoyo:true,
+            //     // repeat: -1,
+            // });
+        }.bind(this));
+
+        this.wavebell.on('start', function () {
+   
+        });
+        this.wavebell.on('stop', function () {
+        
+        });
     }
 }
 
@@ -34,20 +146,17 @@ class RenderScene extends Phaser.Scene{
 
         Phaser.Geom.Mesh.GenerateGridVerts({
             mesh,
-            widthSegments: 6
+            widthSegments: 20
         });
 
         mesh.hideCCW = false;
-        mesh.modelRotation.set(2.604, 0.427, 0);
-        mesh.viewPosition.set(0, 0, 2.833);
+        mesh.modelRotation.set(2.5, 0.0, 0.0);
+        mesh.viewPosition.set(0, 0, 1);
 
         const rotateRate = 1;
-        const panRate = 1;
+        const panRate = 2;
         const zoomRate = 4;
 
-        window.mesh = mesh;
-
-        this.add.text(16, 16, 'Drag mouse to Rotate (+ Shift to pan)\nWheel to zoom');
 
         this.input.on('pointermove', pointer => {
 
@@ -70,30 +179,27 @@ class RenderScene extends Phaser.Scene{
         });
 
         this.input.on('wheel', (pointer, over, deltaX, deltaY, deltaZ) => {
-
-            mesh.panZ(deltaY * (zoomRate / 600));
-
+            mesh.panZ(deltaY * (zoomRate / 1000));
         });
 
-        var tween = this.tweens.add({
-            targets: mesh,
-            rotationY: 10,
-            ease: 'Power1',
-            duration: 3000,
-            yoyo: true,
-            // repeat: 1,
-            onStart: function () { console.log('onStart'); console.log(arguments); },
-            onComplete: function () { console.log('onComplete'); console.log(arguments); },
-            onYoyo: function () { console.log('onYoyo'); console.log(arguments); },
-            onRepeat: function () { console.log('onRepeat'); console.log(arguments); },
-        });
+        // var tween = this.tweens.add({
+        //     targets: mesh,
+        //     rotationY: 10,
+        //     ease: 'Power1',
+        //     duration: 3000,
+        //     yoyo: true,
+        //     // repeat: 1,
+        //     // onStart: function () { console.log('onStart'); console.log(arguments); },
+        //     // onComplete: function () { console.log('onComplete'); console.log(arguments); },
+        //     // onYoyo: function () { console.log('onYoyo'); console.log(arguments); },
+        //     // onRepeat: function () { console.log('onRepeat'); console.log(arguments); },
+        // });
     }
 
     update(time, delta){
         var gameScene = this.scene.get('MainScene');
-console.log("delta")
         this.rt.clear();
 
-        this.rt.draw(gameScene.children, 0, 0);
+        this.rt.draw(gameScene.children, 100, 0);
     }
 }
--- Gom/app.js
+++ Gom/app.js
@@ -1,9 +1,11 @@
 const WIDTH = window.innerWidth;
 const HEIGHT = window.innerHeight;
+const SEED = new Date().getTime();
+
 const CONFIG = {
     type: Phaser.AUTO,
-    backgroundColor: '#000000',
-    scene: [MainScene,RenderScene],
+    backgroundColor:'0xffffff',
+    scene: [MainScene],
     scale: {
         mode: Phaser.Scale.FIT,
         width: WIDTH,
@@ -11,5 +13,9 @@ const CONFIG = {
         autoCenter: Phaser.Scale.CENTER_BOTH
     },
     pixelArt: true,
+    seed: SEED,
+    dom: {
+        createContainer: true
+    }
 };
 const GAME = new Phaser.Game(CONFIG);
